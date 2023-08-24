dcfb15c16b47e32f1dee83b9d2aca5b15b1ade52
chandler05 <66492208+chandler05@users.noreply.github.com>
2023-08-23 19:51:56 -0500

    Lock all booster packs and give trees after each quest

--- Mod.cs
+++ Mod.cs
@@ -3,13 +3,42 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-namespace ExampleModNS
+namespace StacklandsRandomizerNS
 {
-    public class ExampleMod : Mod
+    public class StacklandsRandomizer : Mod
     {
+        public void Awake() {
+            Logger.Log("Awake!");
+            Harmony = new Harmony("com.github.stacklandsrandomizer");
+            InterceptQuestComplete.Logger = Logger;
+            Harmony.PatchAll();
+        }
         public override void Ready()
         {
             Logger.Log("Ready!");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldManager), "QuestCompleted")]
+    public class InterceptQuestComplete {
+        public static ModLogger Logger;
+        public static void Prefix() {
+            Logger.Log("QuestComplete!");
+            WorldManager.instance.CreateCard(WorldManager.instance.CurrentBoard.NormalizedPosToWorldPos(new Vector2(0.6f, 0.5f)), "tree", false, false, true);
+        }
+    }
+
+    [HarmonyPatch(typeof(QuestManager), "JustUnlockedPack")]
+    public class StopGameFromUnlockingPacks {
+        static void Postfix(ref BoosterpackData __result) {
+            __result = null;
+        }
+    }
+
+    [HarmonyPatch(typeof(QuestManager), "BoosterIsUnlocked")]
+    public class LockPacks {
+        static bool Prefix() {
+            return false;
         }
     }
 }
